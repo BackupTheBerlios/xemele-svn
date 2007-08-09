f8f598dda3f0bfd6be936258e756cb559ec7d7e7
thiago_silva <thiago_silva@14e2c336-9c36-0410-94ea-a7cd32d452e3>
2007-08-09 04:26:46 +0000

    -Added copyright noticed -Bot loads it's buddy list (rosterHandler) -Connection code moved to start() -Command messages are trim'ed now -Using accessor methods instead of using foreign object's instance attributes

--- trunk/xemele_bot/xemele_bot.py
+++ trunk/xemele_bot/xemele_bot.py
@@ -1,38 +1,44 @@
 #!/usr/bin/python
-
-## vim:
+##
+#   Copyright (C) 2007 by Thiago Silva <tsilva@sourcecraft.info>
 #
-# :set shiftwidth=2 "using 2 spaces for identation"
-# :set tabstop=2    "two spaces per tab"
-# :set expandtab    "using tabs as spaces"
-# :retab            "convert existing tabs to spaces"
-
-
-## Adding commands to the BOT
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
 #
-# simply define a method starting with 'cmd_'
-#    example: def cmd_say_hi(self, sender, message):
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
 #
-# The above method will be called whenever someone sends a message 'say_hi'
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 #
+# Public License can be found at http://www.gnu.org/copyleft/lgpl.html
+##
 
 import sys
 import os
 import xmpp
-import re
-from config import Config
+from config   import Config
 from commands import Commands
 
 class Bot:
   def __init__(self):
-    config = Config()
+    self.buddylist = {}
+    self.commands = Commands(self)
+
+
+  def start(self):
+    config         = Config()
 
     jid      = xmpp.JID(config['bot_jid'])
     user     = jid.getNode()
     server   = jid.getDomain()
 
     self.con = xmpp.Client(server, debug=[])
-
     conres = self.con.connect()
     if not conres:
         raise ("Unable to connect to server %s!" % server)
@@ -46,13 +52,21 @@ class Bot:
         raise "Unable to authorize on %s - check login/password." % server
         sys.exit(1)
     if authres<>'sasl':
-        raise ("Warning: unable to perform SASL auth os %s. Old " + 
+        raise ("Warning: unable to perform SASL auth os %s. Old " +
         "authentication method used!" % server)
 
-  def start(self):
-    self.con.RegisterHandler('message',self.handler)
-    self.con.sendInitPresence()
+    self.con.RegisterHandler('message',self.messageHandler)
+    self.con.RegisterHandler('iq',self.rosterHandler, ns="jabber:iq:roster")
+    self.con.sendInitPresence() # requestRoster=1
+
+    #main loop
     while self.loop(): pass
+
+  def getBuddyList(self):
+    return self.buddylist
+
+  def connection(self):
+    return self.con
 
   def loop(self):
     try:
@@ -60,11 +74,28 @@ class Bot:
     except KeyboardInterrupt: return False
     return True
 
+  def rosterHandler(self, con, mess):
+    """load the Bot's buddylist"""
+    items = mess.getChildren()[0].getChildren()
+    for item in items:
+      if item.getAttr('subscription') == 'both':
+        self.addBuddy(item.getAttr('jid'))
+      else:
+        if item.getAttr('subscription') == 'remove':
+          self.removeBuddy(item.getAttr('jid'))
+
+  def addBuddy(self, jid):
+    if not jid in self.buddylist:# .has_kay(jid):
+      self.buddylist[jid] = xmpp.JID(jid)
+
+  def removeBuddy(self, jid):
+    del self.buddylist[jid]
+
   def sendTo(self, to, msg):
     self.con.send(xmpp.Message(to, msg))
 
-  def handler(self, con, mess):
-    msg    = mess.getBody()
+  def messageHandler(self, con, mess):
+    msg    = mess.getBody().strip()
     sender = mess.getFrom()
 
     if ' ' in msg:
@@ -74,25 +105,15 @@ class Bot:
 
     cmd = command.lower()
 
-    objCommands = Commands()
-
-    if objCommands.commands.has_key(cmd):
-      reply = objCommands.commands[cmd](self.con, sender, args)
+    if self.commands.supports(cmd):
+      reply = self.commands.do(cmd, sender, args)
     else:
       reply = 'command not found: "%s". try "help"' % ( cmd, )
 
     if reply:
       self.sendTo(sender, reply)
-
-  ### Helpers ###
-
-  def assign(self, assigner, to, url):
-    self.sendTo(jid, 'confirm_assign from=' + assigner + " atom=" + url)
-
-
-  # end #
+# end #
 
 print "bot started"
 Bot().start()
 print "bot killed"
-
